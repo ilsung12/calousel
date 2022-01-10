b70d81de21bd8f2900c637196c87b0528f902f1b
coil <75682926+ilsung12@users.noreply.github.com>
2022-01-11 00:25:46 +0900

    📝 Learn : Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Carousel from './components/Carousel';
 
 function App() {
-  return <div>hello calousel!</div>;
+  return (
+    <>
+      <Carousel />
+    </>
+  );
 }
 
 export default App;
